d56016f8cc5398cfec52da431ceb23c5a7e07693
Sky Johnson <skylearj@gmail.com>
2024-06-29 11:52:40 -0500

    Update community page

--- public/community/index.php
+++ public/community/index.php
@@ -1,25 +1,13 @@
 <?php
-	define('SAFE', true);
-	
-	$LockedPages = array();
-	
-	include("Pieces/header.htm");
-	
-	if (!empty($_GET['page'])) {
-		$page = "index";
-		
-		$TmpPage = basename($_GET['page']);
-		
-		// If it's not a disallowed path, and if the file exists, update $page
-		if (!in_array($TmpPage, $LockedPages) && file_exists("Pieces/Community/{$TmpPage}.htm")) {
-			$page = $TmpPage;
-		}
-	} else {
-		$page = "index";
-	}
-	
-	// Include $page
-	include("Pieces/Community/$page.htm");
-	
-	include("Pieces/footer.htm");
-?>
+
+require '../app/bootstrap.php';
+
+const PAGES = ['login', 'verify', 'register'];
+
+echo render('header');
+
+$page = !empty($_GET['page']) && in_array($_GET['page'], PAGES) ? $_GET['page'] : 'home';
+
+echo render("community/$page");
+
+echo render('footer');
